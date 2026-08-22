c41e6f9e1181ef3f3fad34ca8671b0da0c074f59
JHubbs05 <166656561+JHubbs05@users.noreply.github.com>
2026-08-22 17:21:28 -0400

    Add files via upload

--- controller.js
+++ controller.js
@@ -8,4 +8,23 @@ const getInventory = (req, res) => {
     });
 };
 
-module.exports = {getInventory,};
+//controller filter based on Id
+const getInventoryById = (req, res) => {
+    const id = parseInt(req.params.id);
+    if(isNaN(id)) {
+        return res.status(400).json({error: "Invalid Value"})
+    }
+    console.log("Received Id");
+    pool.query(queries.getInventoryById, [id], (error, results) => {
+        if(error){
+            console.error("Database Error", error);
+            return res.status(500).json({error: "Not Found"});
+        }
+        res.status(200).json(results.rows);
+    });
+}
+
+module.exports = {
+    getInventory,
+    getInventoryById,
+};
--- routes.js
+++ routes.js
@@ -4,6 +4,7 @@ const controller = require("./controller");
 const router = Router();
 
 router.get('/', controller.getInventory);
+router.get('/:id', controller.getInventoryById);
 
 module.exports = router;
 
